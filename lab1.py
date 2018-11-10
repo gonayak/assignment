338043579cf2845fdb6507c97b85a2666a486674
gonayak <44925857+gonayak@users.noreply.github.com>
2018-11-10 20:30:41 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -11,7 +11,8 @@ format_zipcode('123456789')
 
 
 '''
-1.For the given input length is les than or equal to 5 then we should format  the input by prefix 0
+test scenarios : 
+1.For the given input length is les than or equal to 5 then we should format  the input by prefix 0 till the length equals to 5.
 2.for the given input if the length is equal to 10 then display as it is
 3.for the  given input if the length is of 9 , then output should be formatted as seperate first 5 digits and remaining with '-'
 4.for any other length zipcode input there should not be any output.
